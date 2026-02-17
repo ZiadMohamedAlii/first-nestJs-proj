8be978c0d2320d59c7ac384c0ae7df36cfb20acc
ziad mohamed hussain <ziadmohamed0002@ziads-MacBook-Pro.local>
2026-02-17 22:27:39 +0200

    new functions are added create-get-upade-delete user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,61 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Query,
+  Delete,
+  Patch,
+} from '@nestjs/common';
+
+import { NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user-dto';
+import { UpdateUserDto } from './dto/update-user-dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  //
+  //get All users
+  @Get()
+  findAllUsers() {
+    return this.usersService.findAll();
+  }
+
+  //create user
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     this.usersService.create(body.email, body.password);
   }
 
+  // get user by id
+  @Get('/:id')
+  async findOne(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      return new NotFoundException('User not found');
+    }
+
+    return user;
+  }
+
+  // delete user
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  // get user by email
   @Get()
-  findOne(@Param() id: number) {
-    this.usersService.findOne(id);
+  findByEmail(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  //update user by id
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -7,20 +7,28 @@ import { User } from './user.entity';
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
+  // find all users
+  findAll() {
+    return this.repo.find();
+  }
+
+  // find user by email
+  find(email: string) {
+    if (!email) {
+      throw new NotFoundException('Email query is required');
+    }
+    return this.repo.find({ where: { email } });
+  }
+
+  //   findOne by id
+  async findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
   // Creating new User
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
     return this.repo.save(user);
-  }
-
-  //   findOne by id
-  findOne(id: number) {
-    return this.repo.findOneBy({ id });
-  }
-
-  //   find single record by email
-  find(email: string) {
-    return this.repo.find({ where: { email } });
   }
 
   //   update user
@@ -28,9 +36,25 @@ export class UsersService {
     const user = await this.findOne(id);
 
     if (!user) {
-      throw new Error('user with that ID is not found');
+      throw new NotFoundException('user with that ID is not found');
     }
+
     Object.assign(user, attrs);
     return this.repo.save(user);
   }
+
+  //   remove user
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('user with that ID is not found');
+    }
+    return this.repo.remove(user);
+  }
+
+  //   find single record by email
+  // find(email: string) {
+  //   return this.repo.find({ where: { email } });
+  // }
 }
