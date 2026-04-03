0fecdb1a1e6986c07e4a12edb40e46fc0025c19d
ziad mohamed hussain <ziadmohamed0002@ziads-MacBook-Pro.local>
2026-04-03 02:45:29 +0200

    adding GetAllReports , getEstmaite and AdminGuard

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -5,7 +5,10 @@ import {
   UseGuards,
   Patch,
   Param,
+  Get,
+  Query,
 } from '@nestjs/common';
+
 import { CreateReportDto } from './dto/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guards';
@@ -14,6 +17,8 @@ import { User } from 'src/users/user.entity';
 import { Serialize } from '../Interceptors/serialize.Interceptors';
 import { ReportDto } from './dto/report.dto';
 import { ApprovedReportDto } from './dto/approve-report.dto';
+import { AdminGuard } from 'src/guards/admin.guard';
+import { GetEstimateDto } from './dto/get-estimate.dto';
 
 @Serialize(ReportDto)
 @UseGuards(AuthGuard)
@@ -22,14 +27,27 @@ export class ReportsController {
   //
   constructor(private reportsService: ReportsService) {}
 
+  //get All Reports for an admin
+  @Get()
+  getAllReports() {
+    return this.reportsService.getAll();
+  }
+
   // create reports
   @Post()
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
   }
 
+  //Get a specific report using query
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportsService.createEstimate(query);
+  }
+
   //updating Report by Id
   @Patch('/:id')
+  @UseGuards(AdminGuard)
   approveReport(@Param('id') id: number, @Body() body: ApprovedReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
   }
